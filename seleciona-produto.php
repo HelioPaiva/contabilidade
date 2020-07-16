8ae4b7bccdfd89b21a56eb733c7d205b11ec17ce
Helio Paiva <hjppaiva@yahoo.com.br>
2020-07-16 12:18:27 -0300

    correcao logout e inicio sessão paginas de produto

--- seleciona-produto.php
+++ seleciona-produto.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION['login'])){
+  session_destroy();
+  header("Location: index.php");
+}
 //require_once 'controle/servico.php';
 //readAll();
 ?>
@@ -112,11 +117,6 @@ if(isset($_GET['r'])){
   </div>
 </div>
 <?php } ?>
-
-
-
-<!-- Modal Logout -->
-<?php include 'logout.php'; ?>
 
 </div>
 
